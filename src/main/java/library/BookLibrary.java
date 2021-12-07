79a9876d8c4ed03288d632fcd1cdc90f6ed31523
t-bear-git <60154712+t-bear-git@users.noreply.github.com>
2021-12-07 19:05:34 +0100

    Update BookLibrary.java

--- src/main/java/library/BookLibrary.java
+++ src/main/java/library/BookLibrary.java
@@ -7,10 +7,7 @@ public class BookLibrary {
     private ArrayList<Book> books = new ArrayList<>();
 
     public BookLibrary() {
-        /*books.add(new Book("Harry Potter and the Philosopher's Stone", "J.K Rowlings", "Fantasy", "1997", false, null, null));
-        books.add(new Book("Harry Potter and the Goblet of Fire", "J.K Rowlings", "Fantasy", "2000", false, null, null));
-        books.add(new Book("High Fidelity", "Nick Hornby", "Comedy", "1995", false, null, null));
-        books.add(new Book("Glöm Mig", "Alex Schulman", "Biografi", "2016", false, null, null));*/
+
     }
 
     public ArrayList<Book> getBooksFromLibrary() {
